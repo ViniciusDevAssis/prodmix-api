85ac4bb5566c2096962970faa001c9e76f74f0d0
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2025-07-21 16:09:24 -0300

    Criação do projeto

---
+++ src/test/java/com/prodmix/api/ProdmixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prodmix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdmixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
