04283400b61374797e9db7c41728d4378d59dcb0
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2025-07-31 16:55:41 -0300

    Correção no método register

--- src/main/java/com/prodmix/api/services/AuthService.java
+++ src/main/java/com/prodmix/api/services/AuthService.java
@@ -5,6 +5,7 @@ import com.prodmix.api.dtos.ResponseStoreDto;
 import com.prodmix.api.entities.Store;
 import com.prodmix.api.mappers.StoreMapper;
 import com.prodmix.api.repositories.StoreRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ public class AuthService {
     private final StoreRepository repository;
     private final StoreMapper mapper;
 
+    @Transactional
     public ResponseStoreDto register(CreateStoreDto dto) {
         Store store = mapper.createStoreDTOToStore(dto);
         Store savedStore = repository.save(store);
