8088ba60798e65aea8046bde2e9fe45a56feb0e5
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2025-08-04 15:26:07 -0300

    ProductMapper

---
+++ src/main/java/com/prodmix/api/mappers/ProductMapper.java
@@ -0,0 +1,25 @@
+package com.prodmix.api.mappers;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+
+import com.prodmix.api.dtos.CreateProductDto;
+import com.prodmix.api.dtos.ResponseProductDto;
+import com.prodmix.api.dtos.UpdateProductDto;
+import com.prodmix.api.entities.Product;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    //Para uma única Product
+    CreateProductDto productToCreateProductDto(Product product);
+    Product createProductDtoProduct(CreateProductDto dto);
+    ResponseProductDto productToResponseProductDto(Product product);
+    Product responseProductDtoToProduct(ResponseProductDto dto);
+    UpdateProductDto productToUpdateProductDto(Product product);
+    Product updateProductDtoToProduct(UpdateProductDto dto);
+
+    //Para uma lista de Products
+    List<ResponseProductDto> productsToListDto(List<Product> products);
+}
